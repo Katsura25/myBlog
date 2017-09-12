de81a27ed3f820951fee44eb2137c84dde39db61
markiki <admin@admin.com>
2017-09-12 00:42:16 -0400

    Modified Files

--- myblog/views.py
+++ myblog/views.py
@@ -2,5 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
+from django.views import generic
 
 # Create your views here.
+def index(request):
+    num_posts = Post.objects.all().count()
+
+#Render the HTML index.html
+    return render(
+        request,
+        'index.html',
+        context = {'num_posts':num_posts},
+    )
+
+class PostListView(generic.ListView):
+    model = Post
+    
